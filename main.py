9e473c3153306c077a13a6c93ebb4e800a60c124
Silmara Ferreira <Silmara-f>
2024-05-02 23:05:10 -0300

    backend com python teste2

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, url_for, render_template
+
+#render_tamplate busca aquivo html envia variávieis para backen
+
+#url_for cria url interna
+#inicialização
+app = Flask(__name__)
+
+
+#rotas
+@app.route("/")
+def ola_mundo():
+    return render_template("index.html")
+#retorno pode ser string,json
+
+
+@app.route("/sobre")
+def pagina_sobre():
+    return """
+<b> Programarpython</b>:assista 
+<a href ="https://youtu.be/lv5a80XgzlI?si=EPPGoBLtQWXxWx_b"> Cannal no youtube</a>
+
+"""
+#execução
+if __name__=="__main__":
+    app.run(debug=True)
